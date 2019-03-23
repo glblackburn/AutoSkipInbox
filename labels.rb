e762e51a443dd2c38925684c7b8d16d2e995dc7e
Lee Blackburn <g.lee.blackburn@gmail.com>
2019-03-23 08:08:55 -0400

    turn down the output

--- labels.rb
+++ labels.rb
@@ -3,12 +3,12 @@ def getLabelsMaps(service, user_id)
   idMap = {}
   ## Show the user's labels
   r = service.list_user_labels(user_id)
-  puts 'Labels:'
-  puts 'No labels found' if r.labels.empty?
+#  puts 'Labels:'
+#  puts 'No labels found' if r.labels.empty?
   r.labels.each { |label|
     nameMap[label.name] = label.id
     idMap[label.id] = label.name
-    puts "label.id=[#{label.id}] - label.name=[#{label.name}]"
+#    puts "label.id=[#{label.id}] - label.name=[#{label.name}]"
   }
   return nameMap, idMap
 end
