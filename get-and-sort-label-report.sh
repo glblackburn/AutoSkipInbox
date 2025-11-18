a74939b0e4d65566744edc844dc1f49f1e11f249
Lee Blackburn <g.lee.blackburn@gmail.com>
2025-11-18 10:00:31 -0500

    save report to log/reports dir.  remove sorting

--- get-and-sort-label-report.sh
+++ get-and-sort-label-report.sh
@@ -1,9 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euET -o pipefail
 
-report_file=get-label-report.txt
+script_name=$(basename $0)
+script_dir=$(dirname $0)
+
+################################################################################
+# CLI Parameters
+################################################################################
+
+################################################################################
+# default values
+################################################################################
+report_dir=${script_dir}/log/reports
+ts=`date +%Y-%m-%d_%H%M%S`
+report_file=${report_dir}/get-label-report_${ts}.txt
+
+mkdir -p ${report_dir}
 
 ruby get-label-report.rb | tee ${report_file}
-cat<<EOF
-================================================================================
-EOF
-cat ${report_file} | sed "s/:/,/g" | grep ",inbox," | sort -t , -k 4 -n
