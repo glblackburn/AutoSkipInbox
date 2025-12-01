15e3695530cec05c5594dae040ce7eedef0d7f6e
Lee Blackburn <g.lee.blackburn@gmail.com>
2025-12-01 09:47:38 -0500

    find latest report as default

--- sort-label-report.sh
+++ sort-label-report.sh
@@ -5,8 +5,14 @@ script_name=$(basename $0)
 script_dir=$(dirname $0)
 
 label=${1:-autoskipinbox}
-file=${2:-log/reports/get-label-report_2025-11-18_091723.txt}
+file=${2:-$(find log/reports -type f -name "get-label-report_*.txt" | sort | tail -1)}
 
+cat<<EOF
+================================================================================
+label=[${label}]
+file=[${file}]]
+================================================================================
+EOF
 grep "label:${label}," ${file} |
 awk -F'count:' '{print $2 "\t" $0}'   |
 sort -nr   | cut -f2-
